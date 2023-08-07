c0ee6fba17bc4f0234b3b0360be35a550d127c93
YuukiNishimoto <yuukinishimoto1109@gmail.com>
2023-08-07 21:57:48 +0900

    [Finish] Topic1-3

---
+++ topic3.py
@@ -0,0 +1,21 @@
+# this is the first comment
+spam = 1  # and this is the second comment
+          # ... and now a third!
+text = "# This is not a comment because it's inside quotes."
+
+#3.1
+print(2+2)
+print(50-5*6)
+print((50-5*6)/4)
+print(8/5)          # classic division returns a float
+print(17/3)
+print(17//3)        # floor division discards the fractional part(整数部分)
+print(17%3)         # the % operator returns the remainder of the division
+print(5**2)         # 5 squared
+print(2**7)         # 2 to the power of 7
+
+word = 'Python'
+print(word[0])
+print(word[-1])
+print(word[:2])
+print(word[:-1])
